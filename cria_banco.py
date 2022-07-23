82307928ea986ca98bb4f66f5634fd68f451aaeb
Edimar Nascimento <edimar.daros@gmail.com>
2022-07-22 22:45:53 -0300

    41. Criando Banco de Dados manualmente com Sqlite3

---
+++ cria_banco.py
@@ -0,0 +1,16 @@
+from multiprocessing import connection
+import sqlite3
+
+connection = sqlite3.connect('banco.db')
+cursor = connection.cursor()
+
+cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY,\
+  nome text, estrelas real, diaria real, cidade text)"
+
+cria_hotel = "INSERT INTO hoteis VALUES ('alpha', 'Alpha Hotel', 4.3, 450.99, 'Criciuma')"
+
+cursor.execute(cria_tabela)
+cursor.execute(cria_hotel)
+
+connection.commit()
+connection.close()
