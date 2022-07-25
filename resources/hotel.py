4a6f610cd437bcaa3158b86666bf3faa1ed50691
Edimar Nascimento <edimar.daros@gmail.com>
2022-07-24 22:02:41 -0300

    60. Normalizando Dados Passados Via Parametros de Consulta

--- resources/hotel.py
+++ resources/hotel.py
@@ -4,6 +4,33 @@ from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
 import sqlite3
 
+def normalize_path_params(cidade=None,
+                          estrelas_min=0,
+                          estrelas_max=0,
+                          diaria_min=0,
+                          diaria_max=10000,
+                          limit=50,
+                          offset=0, **dados):
+  if cidade:
+    return {
+      'estrelas_min' : estrelas_min,
+      'estelas_max': estrelas_max,
+      'diaria_min' : diaria_min,
+      'diaria_max' : diaria_max,
+      'cidade': cidade,
+      'limit': limit,
+      'offset':offset
+    }
+  return {
+    'estrelas_min' : estrelas_min,
+      'estelas_max': estrelas_max,
+      'diaria_min' : diaria_min,
+      'diaria_max' : diaria_max,
+      'limit': limit,
+      'offset':offset
+  }
+
+
 # path /hoteis?cidade=Criciuma&estrelas_min=4&diaria_max=400
 path_params = reqparse.RequestParser()
 path_params.add_argument('cidade', type=srt)
