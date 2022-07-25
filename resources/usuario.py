c5c08b82caab825ff1081f13609df8db50379a46
Edimar Nascimento <edimar.daros@gmail.com>
2022-07-24 21:07:57 -0300

    52. Implementando Cadastro de Usuários e Testando Aplicação

--- resources/usuario.py
+++ resources/usuario.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
 class User(Resource):
-
+  # /usuarios/{user_id}
   def get(self, user_id):
     user = UserModel.find_user(user_id)
     if user:
@@ -20,3 +20,18 @@ class User(Resource):
 
       return {'message': 'User Deleted'}
     return {'message': 'User not found'}, 404
+
+class UserRegister(Resource):
+  # /cadastro
+  def post(self):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('login', type=str, required=True, help="The field 'login' can not be empty")
+    atributos.add_argument('senha', type=str, required=True, help="The field 'senha' can not be empty")
+    dados = atributos.parse_args()
+
+    if UserModel.find_by_login(dados['login']):
+      return {'message':"The login '{}' already exists.".format(dados['login'])}
+
+    user = UserModel(**dados)
+    user.save_user()
+    return {'message': 'User created successfully!'}, 201 # 201 created
